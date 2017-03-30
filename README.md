9c45bf28698ad2755aab22532e82528bb52ef747
Nayan <reza.shahreare@gmail.com>
2017-03-30 10:25:23 +0600

    packagist version

---
+++ README.md
@@ -0,0 +1,115 @@
+## Imagesolution
+
+A complete solution for all kind of image operation. 
+
+## Current Version
+
+Release 1.0.0
+
+## Installation
+
+Download the repository and put the class file in your project. include the file in your code and create imagesolution object.
+````
+require_once 'imagesolution.class.php';
+$image = new Imagesolution('asset/twitter.jpg');
+````
+
+## Code Example
+
+There are several functionality this class can do.
+
+###View Image Dimension ###
+**getDim()** function return an associative array with height and width of the image.
+```
+$val = $image->getDim();
+echo $val['width']; // print width of the image
+echo $val['height']; // print height of the image
+
+```
+ 
+###View Image Type###
+**getFileType()** function return the image type(extension) of the image.
+
+```
+echo $image->getFileType(); // print jpg/png/gif
+
+```
+
+###Convert Image To PNG###
+To convert the image to png file from any file type use **convertToPNG()** function. This function will convert the image to png file with same name and same directory. Write permission is required on the directory to perform this operation.
+
+```
+$image->convertToPNG()
+
+```
+###Convert Image To JPG###
+ To convert the image to jpg file from any file type use **convertToJPG()** function. This function will convert the image to jpg file with same name and same directory. Write permission is required on the directory to perform this operation.
+
+```
+$image->convertToJPG()
+
+```
+###Convert Image To GIF###
+ To convert the image to gif file from any file type use **convertToGIF()** function. This function will convert the image to gif file with same name and same directory. Write permission is required on the directory to perform this operation.
+
+```
+$image->convertToGIF()
+
+```
+###Resize Image###
+**resize()** function is for resizing the current image. This function needs two parameter width and height. By default maintaining aspect ratio is turned off. to maintain aspect ratio you have to add optional third parameter. In case of resize with aspect ratio, height and width will be calculated according to the image using given width and height as maximum value. Write permission is required on the directory to perform this operation.
+
+```
+$image->resize($width, $height); // without aspect ratio
+$image->resize($width, $height,1); // with aspect ratio
+
+```
+###Image FitToWidth###
+ **fitToWidth()** function is a variation of the resize() function. in this function you have to put the width of the image as parameter. this function will calculate the height according to the new width and resize the image. Write permission is required on the directory to perform this operation.
+
+```
+$image->fitToWidth($width);
+
+```
+###Image FitToHeight###
+ **fitToHeight()** is almost same as fitToWidth() function. As name suggest, instead of width it works on given height. Write permission is required on the directory to perform this operation.
+```
+$image->fitToHeight($height);
+
+```
+###Make Thumbnail###
+ **thumbnail()** function is to make a  small square image. By default it make 100x100 image. To use different length you can put your desire value in the parameter. Write permission is required on the directory to perform this operation.
+
+```
+$image->thumbnail();
+$image->thumbnail($width);
+
+```
+###Rename Image###
+ To change the name of image use rename() function. this will change the name of the image. newfilename should be only name(no directory and no extention). this function keeps same location and image type. Write permission is required on the directory to perform this operation.
+```
+$image->rename($newfilename);
+
+```
+###Copy Image###
+As name suggest, this function is to create a copy  of the image with given name. this function copy the image and keeps in the same location. Write permission is required on the directory to perform this operation.
+```
+$image->copyImage($filename);
+
+```
+
+
+## TODO
+
+This is a work in progress. The class will be expanded from time to time.
+
+## Contributors
+
+- Report issues
+- Open pull request with improvements
+- Spread the word
+- Reach out to me directly at [A H M Reza Shahreare Khan](http://www.shahreare.me)
+
+## License
+
+MIT © [A H M Reza Shahreare Khan](http://www.shahreare.me)
---
+++ src/imagesolution.class.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This is a class for several type of image manipulation.
+ *
+ * @package shahreare\imagesolution
+ * @class Imagesolution
+ * @version 1.0.0
+ * @author A H M Reza Shahreare Khan, www.shahreare.me
+ */
+namespace shahreare\imagesolution;
+
+class Imagesolution {
+	protected $filename;
+	protected $dimX;
+	protected $dimY;
+	protected $filetype;
+
+	function __construct($file) {
+		$this->filename = $file;
+
+		$resulation = getimagesize($file);
+		$this->dimX = $resulation[0];
+		$this->dimY = $resulation[1];
+
+		$this->filetype = pathinfo($file);
+	}
+
+	function getFilename() {
+		return $this->filename;
+	}
+
+	function setFilename($filename) {
+		$this->filename = $filename;
+	}
+
+	function getFileType() {
+		return $this->filetype['extension'];
+	}
+
+	function setFileType() {
+		$this->filetype = pathinfo($this->filename);
+	}
+
+	function getDim() {
+		return array('width' => $this->dimX, 'height' => $this->dimY);
+	}
+	function setDim($width, $height) {
+		$this->dimX = $width;
+		$this->dimY = $height;
+	}
+
+	function convertToPNG() {
+		$img = imagecreatefromstring(file_get_contents($this->filename));
+		$save = imagepng($img, $this->filetype['dirname'] . '/' . $this->filetype['filename'] . '.png');
+
+		if ($save) {
+			unlink($this->filename);
+			$this->setFilename($this->filetype['dirname'] . '/' . $this->filetype['filename'] . '.png');
+			$this->setFileType();
+		}
+	}
+
+	function convertToJPG() {
+		$img = imagecreatefromstring(file_get_contents($this->filename));
+		$save = imagejpeg($img, $this->filetype['dirname'] . '/' . $this->filetype['filename'] . '.jpg');
+		if ($save) {
+			unlink($this->filename);
+			$this->setFilename($this->filetype['dirname'] . '/' . $this->filetype['filename'] . '.jpg');
+			$this->setFileType();
+		}
+	}
+
+	function convertToGIF() {
+		$img = imagecreatefromstring(file_get_contents($this->filename));
+		$save = imagegif($img, $this->filetype['dirname'] . '/' . $this->filetype['filename'] . '.gif');
+		if ($save) {
+			unlink($this->filename);
+			$this->setFilename($this->filetype['dirname'] . '/' . $this->filetype['filename'] . '.gif');
+			$this->setFileType();
+		}
+	}
+
+	function resize($width, $height, $aspect_ratio = null) {
+		if ($aspect_ratio) {
+			if ($this->dimX / $this->dimY < $width / $height) {
+				$width = round(($this->dimX / $this->dimY) * $height);
+			} else if ($this->dimX / $this->dimY > $width / $height) {
+				$height = round($this->dimY * $width / $this->dimX);
+			}
+		}
+
+		$dst = imagecreatetruecolor($width, $height);
+		if ($this->filetype['extension'] == 'jpg') {
+			$src = imagecreatefromjpeg($this->filename);
+			imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $this->dimX, $this->dimY);
+			//unlink($this->filename);
+			$save = imagejpeg($dst, $this->filetype['dirname'] . '/' . $this->filetype['filename'] . '_temp.jpg');
+			if ($save) {
+				unlink($this->filename);
+				rename($this->filetype['dirname'] . '/' . $this->filetype['filename'] . '_temp.jpg', $this->filename);
+			}
+		} else if ($this->filetype['extension'] == 'png') {
+			$src = imagecreatefrompng($this->filename);
+			imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $this->dimX, $this->dimY);
+			//unlink($this->filename);
+			$save = imagepng($dst, $this->filetype['dirname'] . '/' . $this->filetype['filename'] . '_temp.png');
+			if ($save) {
+				unlink($this->filename);
+				rename($this->filetype['dirname'] . '/' . $this->filetype['filename'] . '_temp.png', $this->filename);
+			}
+		} else if ($this->filetype['extension'] == 'gif') {
+			$src = imagecreatefromgif($this->filename);
+			imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $this->dimX, $this->dimY);
+			//unlink($this->filename);
+			$save = imagegif($dst, $this->filetype['dirname'] . '/' . $this->filetype['filename'] . '_temp.gif');
+			if ($save) {
+				unlink($this->filename);
+				rename($this->filetype['dirname'] . '/' . $this->filetype['filename'] . '_temp.gif', $this->filename);
+			}
+		}
+
+		$this->setDim($width, $height);
+	}
+
+	public function fitToWidth($width) {
+		$height = round($this->dimY * $width / $this->dimX);
+		return $this->resize($width, $height);
+	}
+	public function fitToHeight($height) {
+		$width = round(($this->dimX / $this->dimY) * $height);
+		return $this->resize($width, $height);
+	}
+	public function thumbnail($dim = 100) {
+		return $this->resize($dim, $dim);
+	}
+	public function rename($newName) {
+		rename($this->filename, $this->filetype['dirname'] . '/' . $newName . "." . $this->filetype['extension']);
+		$this->setFilename($newName);
+		return true;
+
+	}
+
+	public function copyImage($newName) {
+		return copy($this->filename, $this->filetype['dirname'] . '/' . $newName . "." . $this->filetype['extension']);
+	}
+
+}
+?>
